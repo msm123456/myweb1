c70cfc7d157cd78a128f698bc1e78db49e685127
mohammadsadra <dr737944@gmail.com>
2020-10-23 00:34:50 +0330

    new data base

--- myweb/myweb1/admin.py
+++ myweb/myweb1/admin.py
@@ -4,9 +4,11 @@ from .models import my_about
 from .models import resume
 from .models import Services
 from .models import skills
+from .models import projects
 # Register your models here.
 admin.site.register(my_home)
 admin.site.register(my_about)
 admin.site.register(resume)
 admin.site.register(Services)
 admin.site.register(skills)
+admin.site.register(projects)
--- myweb/myweb1/views.py
+++ myweb/myweb1/views.py
@@ -5,12 +5,14 @@ from .models import my_about
 from .models import resume
 from .models import Services
 from .models import skills
+from .models import projects
 def home(request):
     home_ob=my_home.objects.all()
     about_ob=my_about.objects.all()
     my_resume=resume.objects.all()
     my_services=Services.objects.all()
     my_skills=skills.objects.all()
+    my_projects=projects.objects.all()
     if request.POST:
         subject=request.POST["subject"]+" from "+request.POST['name']
         message=request.POST["message"]
@@ -22,9 +24,9 @@ def home(request):
             ['dr737944@gmail.com'],
         #fail_silently=True
         )
-        return render(request,'index.html',{"home_ob":home_ob,"about_ob":about_ob,"my_services":my_services,"my_resume":my_resume,"my_skills":my_skills})
+        return render(request,'index.html',{"home_ob":home_ob,"about_ob":about_ob,"my_services":my_services,"my_resume":my_resume,"my_skills":my_skills,"my_projects":my_projects})
     else:
-        return render(request,'index.html',{"home_ob":home_ob,"about_ob":about_ob,"my_services":my_services,"my_resume":my_resume,"my_skills":my_skills})
+        return render(request,'index.html',{"home_ob":home_ob,"about_ob":about_ob,"my_services":my_services,"my_resume":my_resume,"my_skills":my_skills,"my_projects":my_projects})
         
 # Create your views here.
 
